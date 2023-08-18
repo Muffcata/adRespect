058e2b3b778392f295a5b7d48effeb99a375ed7b
Muffcata <marta.oched@gmail.com>
2023-08-19 00:47:30 +0200

    section photos/added masonry library/grid styles

---
+++ src/js/masonry.js
@@ -0,0 +1,9 @@
+import Masonry from 'masonry-layout';
+
+document.addEventListener('DOMContentLoaded', function () {
+  const grid = document.querySelector('.grid');
+  const masonry = new Masonry(grid, {
+    itemSelector: '.grid-item',
+    gutter: 43,
+  });
+});
