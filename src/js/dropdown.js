97dea9b193f3276a24b6f0f07b79cdf1ee570951
Muffcata <marta.oched@gmail.com>
2023-08-20 17:21:36 +0200

    dropdown done

---
+++ src/js/dropdown.js
@@ -0,0 +1,15 @@
+import 'bootstrap';
+
+// document.addEventListener('DOMContentLoaded', function () {
+//   const dropdownMenu = document.getElementById('.dropdown');
+
+//   dropdown.addEventListener('click', function () {
+//     dropdow.classList.toggle('show');
+//   });
+
+//   window.addEventListener('click', function (event) {
+//     if (!dropdownMenu.contains(event.target)) {
+//       dropdownMenu.classList.remove('show');
+//     }
+//   });
+// });
